0d8235a8586c0117069590bd3c379de7e2df9ac8
AN1106 <30297454+AdiNar1106@users.noreply.github.com>
2019-08-21 11:14:01 +0530

    Update auth_page.dart

--- eyespace/lib/pages/auth_page.dart
+++ eyespace/lib/pages/auth_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:eyespace/containers/auth_button/auth_button_container.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class AuthPage extends StatelessWidget {
   @override
@@ -8,6 +9,22 @@ class AuthPage extends StatelessWidget {
     return new Scaffold(
       body: new Stack(
         children: <Widget>[
+          new SizedBox(
+            width: 20.0,
+            height: 500.0,
+            child: ColorizeAnimatedTextKit(
+      text: [
+        "EyeSpace",
+      ],
+      textStyle: TextStyle(fontSize: 50.0, fontFamily: "Horizon"),
+      colors: [
+        Colors.purple,
+        Colors.blue,
+        Colors.yellow,
+        Colors.red,
+      ],
+    ),
+  ),
           new Container(
             alignment: Alignment.center,
             width: pageSize.width,
@@ -23,13 +40,16 @@ class AuthPage extends StatelessWidget {
                 ],
               ),
             ),
+
             child: new Container(
-              height: 400.0,
+              height: 200.0,
               child: new Column(
                 children: <Widget>[
+                  
                   new Padding(
                     padding: const EdgeInsets.only(top: 128.0),
                     child: new GoogleAuthButtonContainer(),
+
                   ),
                 ],
               ),
@@ -39,4 +59,66 @@ class AuthPage extends StatelessWidget {
       ),
     );
   }
+
+/*
+Widget logo(BuildContext context){
+      return Padding(
+        padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.15),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: 220,
+          child: Stack(
+            children: <Widget>[
+              Positioned(
+                  child: Container(
+                child: Align(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle, color: Colors.white),
+                    width: 150,
+                    height: 150,
+                  ),
+                ),
+                height: 154,
+              )),
+              Positioned(
+                child: Container(
+                    height: 154,
+                    child: Align(
+                      child: Text(
+                        "Eyespace",
+                        style: TextStyle(
+                          fontSize: 120,
+                          fontWeight: FontWeight.bold,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                    )),
+              ),
+              Positioned(
+                width: MediaQuery.of(context).size.width * 0.15,
+                height: MediaQuery.of(context).size.width * 0.15,
+                bottom: MediaQuery.of(context).size.height * 0.046,
+                right: MediaQuery.of(context).size.width * 0.22,
+                child: Container(
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.white),
+                ),
+              ),
+              Positioned(
+                width: MediaQuery.of(context).size.width * 0.08,
+                height: MediaQuery.of(context).size.width * 0.08,
+                bottom: 0,
+                right: MediaQuery.of(context).size.width * 0.32,
+                child: Container(
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+  */
 }
